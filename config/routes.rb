fb9d4626d8466cc7bab9f1fd16870d1d05500b91
kenta-0126 <garake_yarou@icloud.com>
2021-06-04 08:08:53 +0000

    [ADD]プロフィール画像の投稿用設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   
   root to: 'homes#top'
+  get 'home/about'
   resources :books, only: [:new, :create, :index, :show, :destroy]
   
   resources :users, only: [:show, :edit, :index]
